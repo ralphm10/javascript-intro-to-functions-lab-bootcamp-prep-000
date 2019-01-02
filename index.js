356bb8970cc2d19758ea9e95c0dd1c4b81374dc4
Ralph Mallett <ralph_mallett@hotmail.com>
2019-01-02 17:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,13 +18,11 @@ function logWhisper(string) {
   function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
   return "I can't hear you!"
-    }
-
- function sayHiToGrandma(string) {
+    
   if (string === string.toUpperCase()){
   return "YES INDEED!"
     }
     
- }
+  }
 }
 
