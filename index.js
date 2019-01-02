05c1fd47211111af96e442361b7b58e9da992604
Ralph Mallett <ralph_mallett@hotmail.com>
2019-01-02 17:34:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) 
-    return "I can't hear you!"
-} else if (string === string.toUpperCase()){
+    return "I can't hear you!"} 
+    
+    else if (string === string.toUpperCase()){
   return "YES INDEED!"  
   
 } else  (string === "I love you, Grandma."()) {
