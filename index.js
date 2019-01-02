ead21de8e314e816b87cbe1dbd397220f4bd14c7
Ralph Mallett <ralph_mallett@hotmail.com>
2019-01-02 17:09:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
 
-
- 
-function sayHiToGrandma(string) {
-  if (string === string.toLowerCase){return "I can't hear you!"
+  function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()){
+  return "I can't hear you!"
     }
-}
 
-}
+
+
