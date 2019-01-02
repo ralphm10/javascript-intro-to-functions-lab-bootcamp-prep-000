caf96c134ca5c022758c971bd8d8376de7d1e8fa
Ralph Mallett <ralph_mallett@hotmail.com>
2019-01-02 16:57:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+ 
+function whisper(string) {
+  return string.toLowerCase()
+} 
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+
+var uppercase = "HELLO!"
+ 
+if (uppercase.toUpperCase() === uppercase) // true
+{return ("YES INDEED!")
+}
+ 
+var lowercase = 'hello!'
+ 
+if (lowercase.toLowerCase() === lowercase) // true
+{return ("I can't hear you!")
+ }
+
+else {
+  return  ("I love you, too")
+}
+  
+}
